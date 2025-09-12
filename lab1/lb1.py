5302b4db74d7a056d25228481f261a19b68b9e00
sdnezz <twinknezz@gmail.com>
2025-09-12 23:15:29 +0300

    LB1 - Task 1 (imShow function)

--- lab1/lb1.py
+++ lab1/lb1.py
@@ -2,5 +2,7 @@ import cv2 as cv
 print(cv.__version__)
 
 tuta_image = cv.imread("input/tytuta.jpg")
-cv.imshow('output', tuta_image)
+cv.namedWindow('Display window', cv.WINDOW_FREERATIO)
+cv.imshow('Display window', tuta_image)
 cv.waitKey(0)
+cv.destroyAllWindows()
