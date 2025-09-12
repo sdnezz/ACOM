df5196822435a5a0dbe039818b8314dba6edd95b
sdnezz <twinknezz@gmail.com>
2025-09-12 23:52:57 +0300

    LB1 - Task 2 (different window properties)

--- lab1/lb1.py
+++ lab1/lb1.py
@@ -1,8 +1,23 @@
 import cv2 as cv
 print(cv.__version__)
 
-tuta_image = cv.imread("input/tytuta.jpg")
-cv.namedWindow('Display window', cv.WINDOW_FREERATIO)
-cv.imshow('Display window', tuta_image)
+#TASK 1
+# tuta_image = cv.imread("input/tytuta.jpg")
+# cv.namedWindow('Display window', cv.WINDOW_FREERATIO)
+# cv.imshow('Display window', tuta_image)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+#TASK 2
+
+tuta_image_pixeled = cv.imread("input/tytuta.jpg", cv.IMREAD_REDUCED_COLOR_8)
+tuta_image_negative = cv.imread("input/tytuta.bmp", cv.IMREAD_COLOR_RGB)
+tuta_image_wb = cv.imread("input/tytuta.png", cv.IMREAD_GRAYSCALE)
+cv.namedWindow('free window ratio', cv.WINDOW_FREERATIO)
+cv.namedWindow('normal window', cv.WINDOW_NORMAL)
+cv.namedWindow('autosize window', cv.WINDOW_AUTOSIZE)
+cv.imshow('free window ratio', tuta_image_pixeled)
+cv.imshow('autosize window', tuta_image_negative)
+cv.imshow('normal window', tuta_image_wb)
 cv.waitKey(0)
 cv.destroyAllWindows()
