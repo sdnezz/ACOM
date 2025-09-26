3cb0c72f53de88e1819eb0538364ae1b7306b85a
sdnezz <twinknezz@gmail.com>
2025-09-26 21:28:47 +0300

    LB2(2) - Treshold filter

--- lab2/lb2.py
+++ lab2/lb2.py
@@ -1,20 +1,43 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-camera = cv2.VideoCapture(0)
+camera = cv.VideoCapture(0)
 
-# task 1
-while camera.isOpened():
-    ok, img= camera.read()
-    if not(ok):
-        break
+def task1():
+    while camera.isOpened():
+        ok, img= camera.read()
+        if not(ok):
+            break
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    cv2.imshow("HSV", img)
+        cv.imshow("HSV", img)
 
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+        if cv.waitKey(1) & 0xFF == 27:
+            break
 
-cv2.destroyAllWindows()
+    cv.destroyAllWindows()
 
+def task2():
+    while camera.isOpened():
+        ok, img = camera.read()
+        if not(ok):
+            break
+
+        HSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+        low_color_red = np.array((100, 90, 100))
+        high_color_red = np.array((140, 190, 190))
+
+        treshold = cv.inRange(HSV, low_color_red, high_color_red)
+
+        cv.imshow("Default", img)
+        cv.imshow("only Red", treshold)
+
+        if cv.waitKey(1) & 0xFF == 27:
+            break
+
+if __name__ == "__main__":
+    # task1()
+    task2()
+    camera.release()
